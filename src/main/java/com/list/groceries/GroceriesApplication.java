a622a50325920e300681fb51c65125fa21abf6b3
bartlomiej.kalka <bartlomiej.kalka@finanteq.com>
2020-12-16 17:07:40 +0100

    init

---
+++ src/main/java/com/list/groceries/GroceriesApplication.java
@@ -0,0 +1,12 @@
+package com.list.groceries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceriesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/list/groceries/GroceriesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.list.groceries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
