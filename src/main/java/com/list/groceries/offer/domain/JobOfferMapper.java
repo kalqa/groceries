770ca4fb22aeaa6fecbef4e126f5b02b5bd7d2cb
bartlomiej.kalka <bartlomiej.kalka@finanteq.com>
2021-01-10 18:03:47 +0100

    used lombok

--- src/main/java/com/list/groceries/offer/domain/JobOfferMapper.java
+++ src/main/java/com/list/groceries/offer/domain/JobOfferMapper.java
@@ -5,7 +5,7 @@ public class JobOfferMapper {
     public JobOffer map(JobOfferResponse response) {
         return JobOffer.builder()
                 .position(response.position)
-                .salary(response.salary.toString() + "PLN")
+                .salary(response.salary.toString())
                 .build();
     }
 }
