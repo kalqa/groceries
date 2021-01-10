49539d7b6560fcaa21cba436517aeb837dfaf8bc
bartlomiej.kalka <bartlomiej.kalka@finanteq.com>
2021-01-10 18:08:53 +0100

    used factory

--- src/main/java/com/list/groceries/offer/domain/JobService.java
+++ src/main/java/com/list/groceries/offer/domain/JobService.java
@@ -1,15 +1,28 @@
 package com.list.groceries.offer.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.list.groceries.offer.infrastructure.HttpClientExample;
 
 public class JobService {
 
     JobOfferMapper jobOfferMapper;
+    JobOfferFactory jobOfferFactory = new JobOfferFactory();
 
     public JobOffer getJobFromService(){
         HttpClientExample httpClient = new HttpClientExample();
         //http call
         JobOfferResponse response = httpClient.get("https://joboffers.com");
         return jobOfferMapper.map(response);
+    }
+
+    public List<JobOffer> getTwoOffers(){
+        final JobOffer javaJobOffer = jobOfferFactory.createJavaJobOffer();
+        final JobOffer pythonJobOffer = jobOfferFactory.createPythonJobOffer();
+        List<JobOffer> jobOffers = new ArrayList<>();
+        jobOffers.add(javaJobOffer);
+        jobOffers.add(pythonJobOffer);
+        return jobOffers;
     }
 }
