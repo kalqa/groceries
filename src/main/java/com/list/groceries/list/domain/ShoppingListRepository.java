780f3bda90555e2b1ba39385ab634af3e66ac522
bartlomiej.kalka <bartlomiej.kalka@finanteq.com>
2021-01-18 13:13:54 +0100

    added @Repository

--- src/main/java/com/list/groceries/list/domain/ShoppingListRepository.java
+++ src/main/java/com/list/groceries/list/domain/ShoppingListRepository.java
@@ -1,6 +1,8 @@
 package com.list.groceries.list.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
 }
