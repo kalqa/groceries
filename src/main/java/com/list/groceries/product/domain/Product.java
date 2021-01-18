5a2a244c20063acb9a33e08ae4f8519eb3497a30
bartlomiej.kalka <bartlomiej.kalka@finanteq.com>
2021-01-18 16:52:27 +0100

    created many to many relation

--- src/main/java/com/list/groceries/product/domain/Product.java
+++ src/main/java/com/list/groceries/product/domain/Product.java
@@ -1,10 +1,15 @@
 package com.list.groceries.product.domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+import com.list.groceries.shoppinglist.domain.ShoppingList;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,9 +20,13 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
     private Long id;
 
     private String name;
 
     private Double price;
+
+    @ManyToMany(mappedBy = "products")
+    List<ShoppingList> productLists;
 }
--- src/main/java/com/list/groceries/shoppinglist/domain/ShoppingList.java
+++ src/main/java/com/list/groceries/shoppinglist/domain/ShoppingList.java
@@ -2,12 +2,14 @@ package com.list.groceries.shoppinglist.domain;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import com.list.groceries.product.domain.Product;
 import lombok.Getter;
@@ -16,15 +18,20 @@ import lombok.Setter;
 @Entity(name = "SHOPPINGLIST")
 @Getter
 @Setter
+public
 class ShoppingList {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "shoppinglist_id")
     private long id;
 
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "id")
+    @ManyToMany
+    @JoinTable(
+            name = "list_products",
+            joinColumns = @JoinColumn(name = "shoppinglist_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
     private List<Product> products;
 }
